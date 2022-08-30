9b867873b870a98df03d3f984e39b4d157823ff0
Max Winther <max.winther@elev.ga.ntig.se>
2022-08-30 10:48:24 +0200

    bhbiuo

---
+++ Lucktext/Program.cs
@@ -0,0 +1,24 @@
+﻿string name;
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+
+string country;
+
+Console.WriteLine("Vilket land bor du i?");
+country = Console.ReadLine();
+4
+
+string freeTime;
+
+Console.WriteLine("Vad gillar du att göra på fritiden?");
+freeTime = Console.ReadLine();
+
+
+
+Console.WriteLine($"Hej! Jag heter {name} och jag bor i {country}. På fritiden så brukar jag {freeTime}!");
+// Console.WriteLine(name);
+
+Console.ReadLine();
+
